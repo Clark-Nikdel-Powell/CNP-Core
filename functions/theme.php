1d54d35c43bfaafc4ec5e5bd9782dd6244668a3e
Chris Roche <rodaine+github@gmail.com>
2013-03-26 14:49:38 -0400

    Schema.Org Helper functions

--- functions/theme.php
+++ functions/theme.php
@@ -11,3 +11,47 @@ function cnp_theme_url($path) {
 	$base = trailingslashit(get_stylesheet_directory_uri());
 	return $base.$path;
 }
+
+//-----------------------------------------------------------------------------
+// SCHEMA.ORG HELPER FUNCTIONS
+//-----------------------------------------------------------------------------
+
+/**
+ * Defines a schema item type
+ * @access public
+ * @param  string $type Upper CamelCase type name
+ */
+function cnp_schema_type($type) {
+	printf(
+		'itemscope itemtype="http://schema.org/%s"',
+		trim($type)
+	);
+}
+
+/**
+ * Defines a schema item property
+ * @access public
+ * @param  string $prop Upper CamelCase property name
+ */
+function cnp_schema_prop($prop) {
+	printf(
+		'itemprop="%s"',
+		trim($prop)
+	);
+}
+
+/**
+ * Defines a schema property that has no corresponding element on the page
+ * as a meta element
+ * 
+ * @access public
+ * @param  string $prop    Upper CamelCase property name
+ * @param  string $content Value of the property
+ */
+function cnp_schema_meta($prop, $content) {
+	printf(
+		'<meta itemprop="%s" content="%s" />',
+		trim($prop),
+		trim($content)
+	);
+}
