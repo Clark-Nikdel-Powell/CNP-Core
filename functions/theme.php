a44b6aec506b65f1e3ee8ae2ff1f62f4e26385df
Chris Roche <rodaine+github@gmail.com>
2013-03-26 13:39:55 -0400

    Docs for cnp_theme_url()

--- functions/theme.php
+++ functions/theme.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Link to files in the theme safely (will account for HTTP/HTTPS)
+ * @access public
+ * @param  string $path Path relative to the theme to a file/directory
+ * @return string       Absolute path to theme resource
+ */
 function cnp_theme_url($path) {
 	$path = ltrim(trim($path), '/');
 	static $base = trailingslashit(get_stylesheet_directory_uri());
