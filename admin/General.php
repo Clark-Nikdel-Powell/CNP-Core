d51c3cf4dab71551af62bdb366427ca0445d6b3c
Chris Roche <rodaine+github@gmail.com>
2013-04-12 11:54:32 -0400

    Hide Uprgrade notices & Enqueue admin style overrides

--- admin/General.php
+++ admin/General.php
@@ -13,9 +13,19 @@ class CNP_Admin_General {
 		<? return ob_get_clean();
 	}
 
+	public static function hide_upgrade_notices() {
+		if (!current_user_can('update_core'))
+			add_filter('pre_site_transient_update_core', function($a) { return null; });
+	}
+
+	public static function enqueue_scripts() {
+		wp_enqueue_style('cnp_admin_styles', CNP_URL.'resources/css/admin.css');		
+	}
 
 	public static function initialize() {
 		add_action('admin_head', array(__CLASS__, 'add_favicon'));
 		add_filter('admin_footer_text', array(__CLASS__, 'admin_footer_text'), 999);
+		add_action('after_setup_theme', array(__CLASS__, 'hide_upgrade_notices'));
+		add_action('admin_enqueue_scripts', array(__CLASS__, 'enqueue_scripts'));
 	}
 }
