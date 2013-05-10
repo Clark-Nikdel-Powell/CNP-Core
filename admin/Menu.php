d37b53058720a5b3e1005ffae6976399065a2ece
Chris Roche <rodaine@gmail.com>
2013-05-10 15:24:59 -0400

    Admin-Only Menu Items

--- admin/Menu.php
+++ admin/Menu.php
@@ -37,6 +37,13 @@ class CNP_Admin_Menu {
 		'link-manager.php',
 	);
 
+	protected static $admin_only_pages = array(
+		'plugins.php',
+		'tools.php',
+		'options-general.php',
+		'themes.php'
+	);
+
 	protected static $submenu_pages_to_remove = array(
 		'themes.php' => array(
 			'theme-editor.php'
@@ -46,11 +53,33 @@ class CNP_Admin_Menu {
 		)
 	);
 
+	protected static $admin_only_subpages = array(
+		'index.php' => array(
+			'update-core.php'
+		)
+	);
+
 	public static function remove_menus() {
 		$menu_pages_to_remove = apply_filters('cnp_menu_pages_to_remove', static::$menu_pages_to_remove);
 		$submenu_pages_to_remove = apply_filters('cnp_submenu_pages_to_remove', static::$submenu_pages_to_remove);
 
-		if (is_array($menu_pages_to_remove)) 
+		$admin_only_pages = apply_filters('cnp_admin_only_pages', static::$admin_only_pages);
+		$admin_only_subpages = apply_filters('cnp_admin_only_subpages', static::$admin_only_subpages);
+
+		$is_admin = in_array('administrator', wp_get_current_user()->roles);
+		if (!$is_admin) {
+			if (is_array($admin_only_pages))
+				$menu_pages_to_remove = is_array($menu_pages_to_remove) 
+					? array_merge($menu_pages_to_remove, $admin_only_pages) 
+					: $admin_only_pages;
+
+			if (is_array($admin_only_subpages))
+				$submenu_pages_to_remove = is_array($submenu_pages_to_remove)
+					? array_merge($submenu_pages_to_remove, $admin_only_subpages)
+					: $admin_only_subpages;
+		}
+
+		if (is_array($menu_pages_to_remove))
 			foreach($menu_pages_to_remove as $page)
 				remove_menu_page($page);
 
