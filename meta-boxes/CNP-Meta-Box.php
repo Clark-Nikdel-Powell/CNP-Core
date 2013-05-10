f50ebd545fd7d49e9fb3382611a6e9c916820812
Chris Roche <rodaine@gmail.com>
2013-05-10 15:59:47 -0400

    Show Metabox Filter; Filter post types; moved nonce in field check; save is an empty function to protect checks from save_post();

--- meta-boxes/CNP-Meta-Box.php
+++ meta-boxes/CNP-Meta-Box.php
@@ -10,6 +10,8 @@ abstract class CNP_Meta_Box {
 
 	protected static $context = false;
 
+	protected static $show_meta_box = true;
+
 	protected static $allowed_contexts = array(
 		'normal',
 		'advanced',
@@ -33,19 +35,31 @@ abstract class CNP_Meta_Box {
 		return static::$id . '-nonce';
 	}
 
+	protected static function post_types() {
+		$post_types = static::$post_type;
+		if (!is_array($post_types)) $post_types = array($post_types);
+		return apply_filters('cnp_meta_box_post_types', $post_types, static::$id);
+	}
+
 	protected static function callback_args() {
 		return static::$callback_args;
 	}
 
 	public static function display($post, $args = null) {
-		wp_nonce_field(static::nonce(), static::nonce());
-		if (static::$fields) CNP_Meta_Box_Field_Factory::display_post_fields(static::$fields, $post->ID);
+		if (static::$fields) {
+			wp_nonce_field(static::nonce(), static::nonce());
+			CNP_Meta_Box_Field_Factory::display_post_fields(static::$fields, $post->ID);
+		}
 	}
 
-	public static function save($post_id) {
+	public static function save($post_id, $post) { }
+
+	public static function save_post($post_id, $post) {
+		if (!in_array($post->post_type, static::post_types())) return $post_id;
 		if (!isset($_POST[static::nonce()]) || !wp_verify_nonce($_POST[static::nonce()], static::nonce())) return $post_id;
 		if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return $post_id;
 		if (static::$fields) CNP_Meta_Box_Field_Factory::save_post_fields(static::$fields, $post_id);
+		static::save($post_id, $post);
 		return $post_id;
 	}
 
@@ -61,8 +75,7 @@ abstract class CNP_Meta_Box {
 		if (!static::$priority || !in_array(static::$priority, static::$allowed_priorities)) 
 			static::$priority = static::$allowed_priorities[0];
 
-		if (!is_array(static::$post_type)) static::$post_type = array(static::$post_type);
-		foreach(static::$post_type as $type) {
+		foreach(static::post_types() as $type) {
 			add_meta_box(
 				static::$id,
 				static::$title,
@@ -75,9 +88,15 @@ abstract class CNP_Meta_Box {
 		}
 	}
 
+	protected static function show_meta_box() {
+		return apply_filters('cnp_show_meta_box', static::$show_meta_box, static::$id);
+	}
+
 	public static function initialize() {
-		add_action('add_meta_boxes', array(get_called_class(), 'add_meta_box'));
-		add_action('save_post', array(get_called_class(), 'save'));
+		if (static::show_meta_box()) {
+			add_action('add_meta_boxes', array(get_called_class(), 'add_meta_box'));
+			add_action('save_post', array(get_called_class(), 'save_post'), 10, 2);
+		}
 	}
 
 }
