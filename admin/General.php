ee47daf6bfc0536a38b4b096022a542850133019
Josh Nederveld <joshnederveld@gmail.com>
2013-07-09 14:57:07 -0400

    Committing ASAP because Chris' hair is on fire.

--- admin/General.php
+++ admin/General.php
@@ -3,12 +3,12 @@
 class CNP_Admin_General {
 
 	public static function add_favicon() {
-		$favicon_url = get_stylesheet_directory_uri() . '/images/icons/favicon.png';
+		$favicon_url = get_stylesheet_directory_uri() . '/images/icons/admin-favicon.ico';
 		echo '<link rel="shortcut icon" href="' . $favicon_url . '" />';
 	}
 
 	public static function admin_footer_text() {
-		ob_start(); ?> 
+		ob_start(); ?>
 			Created by <a href="http://clarknikdelpowell.com/">Clark Nikdel Powell</a>. Powered by <a href="http://wordpress.org">WordPress</a>.
 		<? return ob_get_clean();
 	}
@@ -27,7 +27,7 @@ class CNP_Admin_General {
 		wp_enqueue_style(
 			'cnp_jquery-ui-smoothness',
 			"//ajax.googleapis.com/ajax/libs/jqueryui/{$ui->ver}/themes/smoothness/jquery-ui.css",
-			false, 
+			false,
 			null
 		);
 		wp_enqueue_style('cnp_admin_styles', CNP_URL.'resources/css/admin.css');
@@ -41,6 +41,7 @@ class CNP_Admin_General {
 	}
 
 	public static function initialize() {
+		add_action('login_head', array(__CLASS__, 'add_favicon'));
 		add_action('admin_head', array(__CLASS__, 'add_favicon'));
 		add_filter('admin_footer_text', array(__CLASS__, 'admin_footer_text'), 999);
 		add_action('after_setup_theme', array(__CLASS__, 'hide_upgrade_notices'));
