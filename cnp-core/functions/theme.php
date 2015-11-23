b2e2df4350eba3d7bd3ce8a5a7e9812c493728e6
Glenn Welser <glenn.welser@gmail.com>
2015-11-23 14:46:37 -0500

    move schema functions to module

--- cnp-core/functions/theme.php
+++ cnp-core/functions/theme.php
@@ -272,50 +272,6 @@ function cnp_description() {
 }
 
 //-----------------------------------------------------------------------------
-// SCHEMA.ORG HELPER FUNCTIONS
-//-----------------------------------------------------------------------------
-
-/**
- * Defines a schema item type
- * @access public
- * @param  string $type Upper CamelCase type name
- */
-function cnp_schema_type($type) {
-	printf(
-		'itemscope itemtype="http://schema.org/%s"',
-		trim($type)
-	);
-}
-
-/**
- * Defines a schema item property
- * @access public
- * @param  string $prop Upper CamelCase property name
- */
-function cnp_schema_prop($prop) {
-	printf(
-		'itemprop="%s"',
-		trim($prop)
-	);
-}
-
-/**
- * Defines a schema property that has no corresponding element on the page
- * as a meta element
- *
- * @access public
- * @param  string $prop    Upper CamelCase property name
- * @param  string $content Value of the property
- */
-function cnp_schema_meta($prop, $content) {
-	printf(
-		'<meta itemprop="%s" content="%s" />',
-		trim($prop),
-		trim($content)
-	);
-}
-
-//-----------------------------------------------------------------------------
 // HIGHEST ANCESTOR
 //-----------------------------------------------------------------------------
 
