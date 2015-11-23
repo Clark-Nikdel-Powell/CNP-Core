ba4df2b68327b789b2fa5158a3150b761e73783b
Glenn Welser <glenn.welser@gmail.com>
2015-11-23 15:48:13 -0500

    move svg function to module

--- cnp-core/functions/theme.php
+++ cnp-core/functions/theme.php
@@ -34,30 +34,6 @@ function cnp_get_subdomain() {
 //-----------------------------------------------------------------------------
 
 /**
- * Take an icon name and return inline SVG for the icon.
- * @param  string $icon_name The ID of the icon in the defs list of the SVG file.
- * @param  string $viewbox   Optional viewbox size.
- * @param  string $echo      To output, or not to output. Set to 0 if using URL-query.
- */
-function cnp_isvg($args) {
-
-	$defaults = array(
-		'icon-name'	=> ''
-	,	'viewbox' 	=> '0 0 32 32'
-	,	'echo'		=> true
-	,	'path'      => cnp_theme_url('/img/icons.svg')
-	);
-
-	$vars = wp_parse_args( $args, $defaults );
-	$icon = '<svg role="img" title="'. $vars['icon-name'] .'" class="icon '. $vars['icon-name'] .'" viewBox="'. $vars['viewbox'] .'"><use xlink:href="'. $vars['path'] .'#'. $vars['icon-name'] .'"></use></svg>';
-	if ( $vars['echo'] == true ) {
-		echo $icon;
-	} else {
-		return $icon;
-	}
-}
-
-/**
  * Get a file's extension
  * @param 	string 	$file	the filename, as a string
  */
